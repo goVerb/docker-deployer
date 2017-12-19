45c1ca72d51337d6e5814771ed259e3eb0368c4c
Darionfman <darionfman@gmail.com>
2017-12-19 14:01:45 -0600

    Adds an ami table by region and sets a default image id if one is not passed to it.

---
+++ src/constants/amiTable.js
@@ -0,0 +1,62 @@
+export const amiTable = {
+  'us-east-1': {
+    id: 'ami-fad25980',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+  },
+  'us-east-2': {
+    id: 'ami-58f5db3d',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+  },
+  'us-west-1': {
+    id: 'ami-62e0d802',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+  },
+  'us-west-2': {
+    id: 'ami-7114c909',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+  },
+  'eu-west-1': {
+    id: 'ami-4cbe0935',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+  },
+  'eu-west-2': {
+    id: 'ami-dbfee1bf',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+  },
+  'eu-west-3': {
+    id: 'ami-d179ceac',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+  },
+  'eu-central-1': {
+    id: 'ami-05991b6a',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'        
+  },
+  'ap-northeast-1': {
+    id: 'ami-56bd0030',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'            
+  },
+  'ap-northeast-2': {
+    id: 'ami-7267c01c',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                
+  },
+  'ap-southeast-1': {
+    id: 'ami-1bdc8b78',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+  },  
+  'ap-southeast-2': {
+    id: 'ami-14b55f76',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+  },
+  'ap-south-1': {
+    id: 'ami-e4d29c8b',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+  },
+  'ca-central-1': {
+    id: 'ami-918b30f5',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+  },
+  'sa-east-1': {
+    id: 'ami-d596d2b9',
+    name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+  }
+}

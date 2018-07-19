4aa2e5695f3d6f7b85494fecc707870b9d044c04
reedyrm <richard.g.reedy@gmail.com>
2018-07-19 17:04:57 -0500

    removed unused reference

--- src/route53Client.js
+++ src/route53Client.js
@@ -3,7 +3,6 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 const AWS = require('aws-sdk');
-const moment = require('moment');
 const BlueBirdPromise = require('bluebird');
 const __ = require('lodash');
 const uuid = require('uuid');

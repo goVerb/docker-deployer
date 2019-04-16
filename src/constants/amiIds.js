ee100c6701f18816b169c1d31a8278d8928d5617
Tyler Holzer <tyler@goverb.com>
2019-04-16 13:29:50 -0500

    SIP-4198: Updates AMI table.

--- src/constants/amiIds.js
+++ src/constants/amiIds.js
@@ -1,12 +1,12 @@
 module.exports = () => {
   const amiTable = {
     'us-east-1': {
-      id: 'ami-fad25980',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+      id: 'ami-007571470797b8ffa',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'us-east-2': {
-      id: 'ami-58f5db3d',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+      id: 'ami-0aa9ee1fc70e57450',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'us-west-1': {
       id: 'ami-62e0d802',
@@ -14,51 +14,51 @@ module.exports = () => {
     },
     'us-west-2': {
       id: 'ami-7114c909',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'eu-west-1': {
-      id: 'ami-4cbe0935',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+      id: 'ami-0b8e62ddc09226d0a',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'eu-west-2': {
-      id: 'ami-dbfee1bf',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+      id: 'ami-0380c676fcff67fd5',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'eu-west-3': {
-      id: 'ami-d179ceac',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'    
+      id: 'ami-0b419de35e061d9df',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'eu-central-1': {
-      id: 'ami-05991b6a',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'        
+      id: 'ami-01b63d839941375df',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'ap-northeast-1': {
-      id: 'ami-56bd0030',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'            
+      id: 'ami-086ca990ae37efc1b',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'ap-northeast-2': {
-      id: 'ami-7267c01c',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                
+      id: 'ami-0c57dafd95a102862',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'ap-southeast-1': {
-      id: 'ami-1bdc8b78',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+      id: 'ami-0627e2913cf6756ed',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },  
     'ap-southeast-2': {
-      id: 'ami-14b55f76',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+      id: 'ami-0d28e5e0f13248294',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'ap-south-1': {
-      id: 'ami-e4d29c8b',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+      id: 'ami-05de310b944d67cde',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'ca-central-1': {
-      id: 'ami-918b30f5',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+      id: 'ami-0835b198c8a7aced4',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     },
     'sa-east-1': {
-      id: 'ami-d596d2b9',
-      name: 'amzn-ami-2017.09.d-amazon-ecs-optimized'                    
+      id: 'ami-09987452123fadc5b',
+      name: 'amzn2-ami-ecs-hvm-2.0.20190301-x86_64-ebs'
     }
   };
   return {
